c8dc24dc83ec05cafaf2d1562dda1287057e9858
ARUP SARKAR <arup.uadda@gmail.com>
2024-05-21 18:21:31 +0530

    update page

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-var compression = require('compression')
-var morgan = require('morgan')
-
+const express = require("express");
+const path = require("path");
+const app = express();
+var compression = require("compression");
+var morgan = require("morgan");
 
 // Middleware to redirect non-www to www
 app.use((req, res, next) => {
-    if (req.headers.host.startsWith('www.')) {
-        const redirectUrl = `https://handymanexpert24.com`;
-        res.redirect(301, redirectUrl);
-    } else {
-        // Redirect to the www version of the URL
-        next()
-    }
-})
-app.use(morgan(':req[x-forwarded-for] :method :url :status :res[content-length] - :response-time ms'))
-app.use(compression())
-app.use(express.static(path.join(__dirname, 'public'), {
-    maxAge: '10d', // Adjust as per your caching needs
-}))
+  if (req.headers.host.startsWith("www.")) {
+    const redirectUrl = `https://handymanexpert24.com`;
+    res.redirect(301, redirectUrl);
+  } else {
+    // Redirect to the www version of the URL
+    next();
+  }
+});
+app.use(
+  morgan(
+    ":req[x-forwarded-for] :method :url :status :res[content-length] - :response-time ms"
+  )
+);
+app.use(compression());
+app.use(
+  express.static(path.join(__dirname, "public"), {
+    maxAge: "1d", // Adjust as per your caching needs
+  })
+);
 
-app.use('*', (req, res) => {
-    res.sendStatus(404)
-})
+app.use("*", (req, res) => {
+  res.sendStatus(404);
+});
 
 // app.get('/robots.txt', (req, res) => {
 //     res.render('index')
@@ -41,7 +46,6 @@ app.use('*', (req, res) => {
 //     res.render('contact')
 // })
 
-
-app.listen(8080, '0.0.0.0', () => {
-    console.log('App running on ', 8080)
-})
+app.listen(8080, "0.0.0.0", () => {
+  console.log("App running on ", 8080);
+});
